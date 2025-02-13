e09b022807338aca880dd291724cd886442631e9
Beatriz Krisan <159500527+Beatrizkrf@users.noreply.github.com>
2025-02-13 08:57:27 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 1 </title>
+</head>
+<body>
+
+    <?php
+        $nome = "hello kitty";
+    ?>
+   <p>
+        Bea bea 
+   </p> 
+
+   <?php 
+        echo "<p> hellooo </p>";
+        echo "<p> Amo a $nome </p> ";
+        echo "<p> Eu e meu amigos adoramos aventuras! ".$nome." </p>";
+    ?>
+</body>
+</html>
