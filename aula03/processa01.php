d161c8f748046db80de7d75ab02970e723d73d1c
Beatriz Krisan <159500527+Beatrizkrf@users.noreply.github.com>
2025-02-27 09:01:14 -0300

    Add files via upload

---
+++ aula03/processa01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome =$_REQUEST ['cxnome'];
+    $idade = $_REQUEST ['cxidade'];
+
+    echo "O Seu nome é: $nome <br>";
+    echo "A sua idade é: $idade <br>";
+
+?>
---
+++ aula03/processa02.php
@@ -0,0 +1,9 @@
+<?php 
+
+    $nome = $_POST ['cxnome'];
+    $mensagem = $_POST ['cxmensagem'];
+
+    echo "O Seu nome é: $nome <br>";
+    echo "A sua mensagem é: $mensagem <br>";
+
+?>
