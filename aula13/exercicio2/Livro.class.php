10521f7f78fb9952d38de0e43f87041ff54f3b2b
Beatriz Krisan <159500527+Beatrizkrf@users.noreply.github.com>
2025-06-17 21:40:14 -0300

    Add files via upload

--- aula13/exercicio2/Livro.class.php
+++ aula13/exercicio2/Livro.class.php
@@ -1,14 +1,52 @@
 <?php
-
-    Class Livro{
+    class Livro{
         private $Titulo;
         private $Autor;
-        private $Disponibilidade;
+        private $Disponivel;
 
-        public function __construct($titulo, $autor, $disponibilidade){
-
+        public function __construct($titulo,$autor,$disponivel){
+            $this->Titulo = $titulo;
+            $this->Autor = $autor;
+            $this->Disponivel = $disponivel;
         }
-    }    
 
+        public function Emprestar(){
+            if($this->Disponivel == true){
+                $this->Disponivel = false;
+            }else{
+                echo "O livro não está disponível";
+            }            
+        }
+        
+        public function Devolver(){
+            $this->Disponivel = true;
+            echo "Obrigado pela devolução";
+        }
+
+        public function getTitulo(){
+            return $this->Titulo;
+        }
+
+        public function getAutor(){
+            return $this->Autor;
+        }
+
+        public function getDisponivel(){
+            return $this->Disponivel;
+        }
+
+        public function ExibirStatus(){
+            $titulo_livro = $this->getTitulo();
+            $autor = $this->getAutor();
+
+            if($this->getDisponivel() == true){
+                $disponivel_livro = "Disponível";
+            }else{
+                $disponivel_livro = "Emprestado";
+            }
+            
+            return "Livro $titulo_livro - $autor está: <br> $disponivel_livro !";
+        }
+    }
 
 ?>
--- aula13/exercicio2/livro.php
+++ aula13/exercicio2/livro.php
@@ -1,6 +1,11 @@
 <?php
 
-    include_once 'Livro.class.php';
+     include_once 'Livro.class.php';
+    $livro = new Livro('Jantar Secreto','Raphael Montes',true);
+
+    echo "Status do livro:  <br>";
+    echo $livro->ExibirStatus();
+    echo "<hr>";
     
 
 ?>
